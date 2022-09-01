4b09420ac4005f35fd8807bce18a43fa78bab8f8
hasibferdous <hasibferdous.ict@gmail.com>
2022-09-02 01:17:03 +0600

    phone not found warning showing

--- js/app.js
+++ js/app.js
@@ -7,6 +7,17 @@ const loadPhones = async(searchText) =>{
 const displayPhones = phones =>{
     const phonesContainer = document.getElementById('phones-container');
     phonesContainer.textContent = '';
+    //display 20 phones only
+    phones = phones.slice(0,20);
+    //display no phones found
+    const noPhone = document.getElementById('no-found-message');
+    if(phones.length == 0){
+        noPhone.classList.remove('d-none')
+    }
+    else {
+        noPhone.classList.add('d-none');
+    }
+    //display all phones
     phones.forEach(phone =>{
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
@@ -33,5 +44,5 @@ document.getElementById('btn-search').addEventListener('click',function(){
 })
 
 
-loadPhones();
+//loadPhones();
 
